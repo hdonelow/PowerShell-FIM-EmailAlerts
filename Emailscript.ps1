96686e389fa1c89fd43460d14bb937aeb61a3756
Harless Donelow <43560793+hdonelow@users.noreply.github.com>
2022-07-29 03:11:29 -0700

    Add files via upload
    Run this if having problems with continuous prompts asking for credential logins, or if emails are not working. Run this in a new file.

---
+++ Emailscript.ps1
@@ -0,0 +1,49 @@
+﻿# Get the credential
+$credential = Get-Credential
+
+## Define the Send-MailMessage parameters
+$mailParams = @{
+    SmtpServer                 = 'smtp-mail.outlook.com'
+    Port                       = '587' # or '25' if not using TLS
+    UseSSL                     = $true ## or not if using non-TLS
+    Credential                 = $credential
+    From                       = 'sample123@outlook.com'
+    To                         = 'YourEmail@gmail.com', 'YourEmail@gmail.com'
+    Subject                    = "ALERT1"
+    Body                       = 'A file has been edited!!!'
+    DeliveryNotificationOption = 'OnFailure', 'OnSuccess'
+}
+
+## Send the message
+Send-MailMessage @mailParams
+
+$mailParams1 = @{
+    SmtpServer                 = 'smtp-mail.outlook.com'
+    Port                       = '587' # or '25' if not using TLS
+    UseSSL                     = $true ## or not if using non-TLS
+    Credential                 = $credential
+    From                       = 'sample123@outlook.com'
+    To                         = 'YourEmail@gmail.com', 'YourEmail@gmail.com'
+    Subject                    = "ALERT2"
+    Body                       = 'A file has been created!!!'
+    DeliveryNotificationOption = 'OnFailure', 'OnSuccess'
+}
+
+## Send the message
+Send-MailMessage @mailParams1
+
+$mailParams2 = @{
+    SmtpServer                 = 'smtp-mail.outlook.com'
+    Port                       = '587' # or '25' if not using TLS
+    UseSSL                     = $true ## or not if using non-TLS
+    Credential                 = $credential
+    From                       = 'sample123@outlook.com'
+    To                         = 'YourEmail@gmail.com', 'YourEmail@gmail.com'
+    Subject                    = "ALERT3"
+    Body                       = 'A file has been deleted!!!'
+    DeliveryNotificationOption = 'OnFailure', 'OnSuccess'
+}
+
+## Send the message
+Send-MailMessage @mailParams2
+
